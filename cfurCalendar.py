99c7f83f308f1aa2d7387733a2047364f2836a6b
Mich Ochieng <michochieng@gmail.com>
2022-04-12 19:36:45 -0700

    Added more widgets, changed label

--- cfurCalendar.py
+++ cfurCalendar.py
@@ -1,24 +1,61 @@
+from cProfile import label
+from calendar import calendar
 from icalendar import Calendar, Event
 from datetime  import datetime
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow,QPushButton
+from PyQt6.QtCore    import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget
 
 import sys
 import re
-
-# Scan specific dir for schedule files
-# For each file
-    #  If the playlist is active this week create an event
-    #  Other wise skip it
 
 # Classes
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle("CFUR Calendar")
-        button = QPushButton("CLick!")
 
-        self.setCentralWidget(button) # Place widget in mainWindow
+        #  Window Attributes
+        self.setWindowTitle("CFUR Calendar")
+        self.setFixedSize(QSize(600,750)) 
+        # Add min max sizes later?
+
+        calendar = CalendarCreator()
+
+        # Widgets
+        self.button = QPushButton("Click!")
+        self.button.clicked.connect(self.buttonClicked)
+
+        self.label = QLabel("CFUR Calendar")
+        labelFont  = self.label.font()
+        labelFont.setPointSize(30)
+        self.label.setFont(labelFont)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+        self.mainInput = QLineEdit()
+        self.mainInput.setPlaceholderText("Enter Radiologik schedule folder path here")
+
+        # Layout
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.mainInput)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Add widgets to window
+        self.setCentralWidget(container)
+
+    def buttonClicked(self):
+        self.button.setText("Clicked")
+        self.button.setEnabled(False)
+
+class CalendarCreator:
+    def __init__(self) -> None:
+        pass
+
+    def test(self):
+        print("Hello")
 
 app = QApplication([]) # Wont be using Command line args so passing []
 

ce79980c71f581d5c6ac061c6adb4ecbe9b3f110
Mich Ochieng <michochieng@gmail.com>
2022-04-12 20:47:42 -0700

    Added buttons and terminal class

--- cfurCalendar.py
+++ cfurCalendar.py
@@ -2,8 +2,8 @@ from cProfile import label
 from calendar import calendar
 from icalendar import Calendar, Event
 from datetime  import datetime
-from PyQt6.QtCore    import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtCore    import QSize, Qt, QRect
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget, QScrollArea
 
 import sys
 import re
@@ -18,27 +18,32 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("CFUR Calendar")
         self.setFixedSize(QSize(600,750)) 
         # Add min max sizes later?
-
         calendar = CalendarCreator()
 
         # Widgets
-        self.button = QPushButton("Click!")
-        self.button.clicked.connect(self.buttonClicked)
-
         self.label = QLabel("CFUR Calendar")
         labelFont  = self.label.font()
         labelFont.setPointSize(30)
         self.label.setFont(labelFont)
         self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.label.setMaximumHeight(50)
 
         self.mainInput = QLineEdit()
         self.mainInput.setPlaceholderText("Enter Radiologik schedule folder path here")
 
-        # Layout
+        self.submitBtn = QPushButton("Generate calendar")
+        self.submitBtn.clicked.connect(self.buttonClicked)
+        self.submitBtn.setMinimumHeight(50)
 
+        self.terminal = Terminal(self)
+        self.terminal.setMaximumHeight(200)
+
+        # Layout
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.mainInput)
+        layout.addWidget(self.submitBtn)
+        layout.addWidget(self.terminal)
 
         container = QWidget()
         container.setLayout(layout)
@@ -47,8 +52,35 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
 
     def buttonClicked(self):
-        self.button.setText("Clicked")
-        self.button.setEnabled(False)
+        # Will be used to update terminal later
+        temp = self.terminal.text() + "\n"
+        self.terminal.setText(temp + "test")
+
+# Used for output terminal
+class Terminal(QScrollArea):
+    def __init__(self,*args,**kwargs) -> None:
+        QScrollArea.__init__(self,*args,**kwargs)
+        
+        self.setWidgetResizable(True)
+
+        content = QWidget(self)
+        self.setWidget(content)
+
+        layout = QVBoxLayout(content)
+        
+        self.label = QLabel(content)
+
+        self.label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+
+        self.label.setWordWrap(True) # Allows for multiline
+
+        layout.addWidget(self.label)
+    # Overridden Functions
+    def setText(self,txt):
+        self.label.setText(txt)
+
+    def text(self) -> str:
+        return self.label.text()
 
 class CalendarCreator:
     def __init__(self) -> None:

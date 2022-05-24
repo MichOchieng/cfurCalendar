35002da179e41ea72f0b32b0314521019512d92e
MichOchieng <MichOchieng@gmail.com>
2022-05-24 14:31:04 -0700

    Changed folder name and removed previous code

---
+++ radiologik-calendar/src/components/upload-area/upload-area.tsx
@@ -0,0 +1,17 @@
+import { Button, Paper } from "@mui/material";
+import React, {useRef, useState} from "react";
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+
+
+const UploadArea = (
+
+) => {
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default UploadArea

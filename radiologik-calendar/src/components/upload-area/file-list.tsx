777d61e22a344f56666bfb8042756d67cf6f3fca
MichOchieng <MichOchieng@gmail.com>
2022-05-24 15:35:57 -0700

    Added file list logic

--- radiologik-calendar/src/components/upload-area/file-list.tsx
+++ radiologik-calendar/src/components/upload-area/file-list.tsx
@@ -1,1 +1,18 @@
-import React from "react";
+import React, { memo, PropsWithChildren } from "react";
+
+import { IFileList } from "./interfaces/IFileList";
+
+const FileList = memo(
+    (props: PropsWithChildren<IFileList>) => (
+        <ul>
+            {props.files.map((file: File) => (
+                <li>
+                    <span>{file.name}</span>{' '}
+                    <span>({Math.round(file.size / 1000)}kb)</span>
+                </li>
+            ))}
+        </ul>
+    )
+)
+
+export default FileList

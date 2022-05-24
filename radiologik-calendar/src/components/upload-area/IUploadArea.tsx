2bfa4db6ec58da49a1345189ad36dd091972c08e
MichOchieng <MichOchieng@gmail.com>
2022-05-24 15:06:41 -0700

    Added onDrop

--- radiologik-calendar/src/components/upload-area/IUploadArea.tsx
+++ radiologik-calendar/src/components/upload-area/IUploadArea.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export interface IUploadArea{
     onDrag?:      () => void
+    onDrop?:      () => void
     onDragIn?:    () => void
     onDragOut?:   () => void
     onDropFiles?: (files: File[]) => void

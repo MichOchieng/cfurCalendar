3079b4faff62a5060aea2db2c3800851bb16e38c
MichOchieng <MichOchieng@gmail.com>
2022-05-25 13:36:46 -0700

    Moved main theme to here

---
+++ radiologik-calendar/src/styles.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme =  createTheme({
+    palette: {
+        mode:'dark',
+        primary: {
+          main: '#000000',
+        },
+        secondary: {
+          main: '#987407',
+        },
+      }
+})

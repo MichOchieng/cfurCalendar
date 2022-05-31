6e2bba99617a3dd856a2f902086d2aeba6663abb
MichOchieng <MichOchieng@gmail.com>
2022-05-30 17:10:02 -0700

    Added handleCancel and handleSave

--- radiologik-calendar/src/components/main-ui/main-ui.tsx
+++ radiologik-calendar/src/components/main-ui/main-ui.tsx
@@ -1,24 +1,24 @@
-import { 
-        AppBar,
-        Box,
-        Button,
-        ButtonGroup,
-        Container,
-        Dialog,
-        DialogActions,
-        DialogContent,
-        DialogContentText,
-        DialogTitle,
-        Grid,
-        Input,
-        Paper, 
-        Typography, 
-        } 
+import {
+    AppBar,
+    Box,
+    Button,
+    ButtonGroup,
+    Container,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Grid,
+    Input,
+    Paper,
+    Typography,
+}
     from "@mui/material";
-import { 
-        useState,
-        useCallback 
-        } 
+import {
+    useState,
+    useCallback
+}
     from "react";
 import classNames from "classnames";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -31,14 +31,14 @@ import { UploadFile } from "@mui/icons-material";
 
 const MainUi = () => {
     // Create states for upload area and uploaded files
-    const [isAreaActive,setIsAreaActive] = useState(false);
-    const [files,setFiles]               = useState<File[]>([]);
+    const [isAreaActive, setIsAreaActive] = useState(false);
+    const [files, setFiles] = useState<File[]>([]);
 
     // Calendar related values
-    const ics          = require('ics');
-    let events: any    = [];
-    const sunday       = moment().day("Sunday").hour(0).minute(0).seconds(0);
-    const [numCalendars,setNumCalendars] = useState(1);
+    const ics = require('ics');
+    let events: any = [];
+    const sunday = moment().day("Sunday").hour(0).minute(0).seconds(0);
+    const [numCalendars, setNumCalendars] = useState(1);
 
     // Settings dialog handlers
     const [open, setOpen] = useState(false);
@@ -47,19 +47,22 @@ const MainUi = () => {
         setOpen(true);
     };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
-
     const handleDecrement = () => {
         if (numCalendars == 1) {
-            console.log("Must at least generate one calendar!"); 
+            console.log("Must at least generate one calendar!");
         }
-        else{
+        else {
             setNumCalendars((numCalendars - 1))
         }
     }
 
+    const handleCancel = () => {
+        setOpen(false);
+    }
+
+    const handleSave = () => {
+        setOpen(false);
+    }
     // Handlers for dragging and dropping on upload area
     const onDragStateChange = useCallback((dragActive: boolean) => {
         setIsAreaActive(dragActive)
@@ -69,7 +72,7 @@ const MainUi = () => {
     }, []);
 
     // Handlers for reading files
-    function readFile (){
+    function readFile() {
         events = [] // Clear events for this scan
         // Loop through files from the upload area
         for (let i = 0; i < files.length; i++) {
@@ -77,67 +80,67 @@ const MainUi = () => {
             reader.onloadend = () => {
                 // Seperate lines of each file
                 let lines = (reader.result as string).split(/\r\n|\n/);
-                let days:       string[] = [];
-                let duration:   number   = 0;
+                let days: string[] = [];
+                let duration: number = 0;
                 let startTimes: number[] = [];
-                let eventName:  string   = '';
-                let eventTimes: [string,number][] = [];
+                let eventName: string = '';
+                let eventTimes: [string, number][] = [];
                 for (let j = 0; j < 23; j++) {
                     // Init vars
-                    switch(j){
+                    switch (j) {
                         // Scheduled days
                         case 1:
                             const tempDays = lines[j].split("/");
                             days = tempDays
                             // Determine the correct days and remove '_'
-                            for (const [index,val] of tempDays.entries()) {
-                                if (val[0] == 'S'){
+                            for (const [index, val] of tempDays.entries()) {
+                                if (val[0] == 'S') {
                                     days[index] = "Su";
                                 }
-                                else if (val[2] == 'T'){
+                                else if (val[2] == 'T') {
                                     days[index] = "Tu";
                                 }
-                                else if (val[4] == 'S'){
+                                else if (val[4] == 'S') {
                                     days[index] = "Sa";
                                 }
-                                else if (val[6] == 'T'){
+                                else if (val[6] == 'T') {
                                     days[index] = "Th";
                                 }
                                 else {
                                     // Only keeps alphabetic chars
-                                    days[index] = days[index].replace(/[^A-Za-z0-9]/g,"") 
+                                    days[index] = days[index].replace(/[^A-Za-z0-9]/g, "")
                                 }
                             }
                             break;
                         // Duration
                         case 2:
-                            duration += parseInt(lines[j].replace(" \n",""))
+                            duration += parseInt(lines[j].replace(" \n", ""))
                             break;
                         //  Start times
                         case 5:
                             // Converts strings of start times to ints
                             let temp1 = lines[j].split("/")
                             for (let index = 0; index < temp1.length; index++) {
-                                startTimes.push(parseInt(temp1[index])/2)
+                                startTimes.push(parseInt(temp1[index]) / 2)
                             }
                             break;
                         // Event name
                         case 22:
                             // Strip the first 43 chars
                             // Put everything in quotation mars in a list and grab the first occurance
-                            let temp   = lines[j].slice(43)
-                            let temp2  = temp.match('"([^"]*?)"')
-                            eventName  = ((temp2 != null) ? temp2[0] : "")
+                            let temp = lines[j].slice(43)
+                            let temp2 = temp.match('"([^"]*?)"')
+                            eventName = ((temp2 != null) ? temp2[0] : "")
                             break;
                     }
                 }
                 // Create a list of 'tuples' with event day and time
-                for (const [index,val] of days.entries()) {
-                    if (val != ''){
-                        eventTimes.push([days[index],startTimes[index]])
+                for (const [index, val] of days.entries()) {
+                    if (val != '') {
+                        eventTimes.push([days[index], startTimes[index]])
                     }
                 }
-                createEvents(eventTimes,eventName,duration)
+                createEvents(eventTimes, eventName, duration)
                 createCalendar();
             };
             let file = files[i];
@@ -145,41 +148,41 @@ const MainUi = () => {
         }
     }
 
-    function createEvents(eventTimes: [string,number][], name: string, duration: number){
+    function createEvents(eventTimes: [string, number][], name: string, duration: number) {
         for (let index = 0; index < eventTimes.length; index++) {
-            let date    = calculateDateTime(eventTimes[index]);
+            let date = calculateDateTime(eventTimes[index]);
             let startdt = date!.format('YYYYMMDDHHmmss'); //Creates an array
             // let enddt   = date?.add(duration,"minutes").format('YYYY-M-D-H-m').split("-");
             console.log("start: " + startdt);
             events.push({
                 title: name,
                 start: [
-                    parseInt(startdt!.slice(0,4)),
-                    parseInt(startdt!.slice(5,6)),
-                    parseInt(startdt!.slice(6,8)),
-                    parseInt(startdt!.slice(9,10)),
-                    parseInt(startdt!.slice(11,12)),
+                    parseInt(startdt!.slice(0, 4)),
+                    parseInt(startdt!.slice(5, 6)),
+                    parseInt(startdt!.slice(6, 8)),
+                    parseInt(startdt!.slice(9, 10)),
+                    parseInt(startdt!.slice(11, 12)),
                 ],
                 duration: {
-                    minutes:duration
+                    minutes: duration
                 },
             });
         }
-        
+
     }
 
     function createCalendar() {
         const { error, value } = ics.createEvents(events);
         if (error) {
-        console.log(error);
-        return;
+            console.log(error);
+            return;
         }
         download(value);
     }
 
-    function download(val:string) {
+    function download(val: string) {
         const element = document.createElement("a");
-        const file = new Blob([val],{
+        const file = new Blob([val], {
             type: "text/plain"
         });
         element.href = URL.createObjectURL(file);
@@ -189,23 +192,23 @@ const MainUi = () => {
     }
 
     // Uses the 'sunday' const to calculate a date time from the incoming event times
-    function calculateDateTime(eventTimes: [string,number]){
+    function calculateDateTime(eventTimes: [string, number]) {
         for (let index = 0; index < eventTimes.length; index++) {
             switch (eventTimes[0]) {
                 case "Su":
-                    return sunday.set("hour",eventTimes[1]);
+                    return sunday.set("hour", eventTimes[1]);
                 case "M":
-                    return moment(sunday).add(1,'day').set("hour",eventTimes[1]);
+                    return moment(sunday).add(1, 'day').set("hour", eventTimes[1]);
                 case "Tu":
-                    return moment(sunday).add(2,'day').set("hour",eventTimes[1]);
+                    return moment(sunday).add(2, 'day').set("hour", eventTimes[1]);
                 case "W":
-                    return moment(sunday).add(3,'day').set("hour",eventTimes[1]);
+                    return moment(sunday).add(3, 'day').set("hour", eventTimes[1]);
                 case "Th":
-                    return moment(sunday).add(4,'day').set("hour",eventTimes[1]);
+                    return moment(sunday).add(4, 'day').set("hour", eventTimes[1]);
                 case "F":
-                    return moment(sunday).add(5,'day').set("hour",eventTimes[1]);
+                    return moment(sunday).add(5, 'day').set("hour", eventTimes[1]);
                 case "Sa":
-                    return moment(sunday).add(6,'day').set("hour",eventTimes[1]);
+                    return moment(sunday).add(6, 'day').set("hour", eventTimes[1]);
                 default:
                     console.log("Error occured calculating event datetime!")
                     break;
@@ -214,7 +217,7 @@ const MainUi = () => {
     }
 
     return (
-        
+
         <Grid
             container
         >
@@ -226,7 +229,7 @@ const MainUi = () => {
                 <AppBar
                     position="static"
                     sx={{
-                        marginBottom:"10vh",
+                        marginBottom: "10vh",
 
                     }}
                 >
@@ -234,20 +237,20 @@ const MainUi = () => {
                         maxWidth="xl"
                     >
                         <Typography
-                            variant="h6" 
-                            component="div" 
-                            sx={{ flexGrow: 1, padding:"0.5em" }}
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1, padding: "0.5em" }}
                         >
                             Radiologik Calendar
-                        </Typography>   
+                        </Typography>
                     </Container>
                 </AppBar>
             </Grid>
             {/* Second row */}
-            <Grid 
+            <Grid
                 item
                 xs={12}
-                
+
             >
                 <Paper
                     className={classNames('uploadArea', {
@@ -256,11 +259,11 @@ const MainUi = () => {
                     sx={{
                         width: "80vw",
                         height: "30vh",
-                        margin:"0 auto",
-                        marginBottom:"10vh",
-                        display:"flex",
-                        alignItems:"center",
-                        justifyContent:"center",
+                        margin: "0 auto",
+                        marginBottom: "10vh",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
                     }}
                 >
                     {/* Upload area and button container */}
@@ -272,34 +275,34 @@ const MainUi = () => {
                             item
                             xs={12}
                             sx={{
-                                display:"flex",
-                                alignItems:"center",
-                                justifyContent:"center",
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
                             }}
                         >
                             <UploadArea
                                 onDragStateChange={onDragStateChange}
                                 onDropFiles={onDropFiles}
                             >
-                            <h2> Drop files here</h2>
+                                <h2> Drop files here</h2>
                                 {files.length === 0 ? (
                                     <h3>No files to scan</h3>
-                                    ) :
+                                ) :
                                     (
                                         <h3>Files to scan: {files.length}</h3>
                                     )
                                 }
-                            <FileList files={files}/>
+                                <FileList files={files} />
                             </UploadArea>
                         </Grid>
                         {/* Buttons */}
                         <Grid
                             item
                             xs={12}
-                            sx={{              
-                                display:"flex",
-                                alignItems:"center",
-                                justifyContent:"center",
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
                                 marginBottom: "1em"
                             }}
                         >
@@ -309,7 +312,7 @@ const MainUi = () => {
                                 color="secondary"
                             >Download
                             </Button>
-                            <Box sx={{width: "25%"}} />
+                            <Box sx={{ width: "25%" }} />
                             <Button
                                 onClick={handleClickOpen}
                                 variant="contained"
@@ -317,15 +320,15 @@ const MainUi = () => {
                             >Settings
                             </Button>
                             {/* Pop up dialog */}
-                            <Dialog 
-                                open={open} 
-                                onClose={handleClose}
+                            <Dialog
+                                open={open}
+                                onClose={handleCancel}
                                 aria-describedby="alert-dialog-slide-description"
                             >
                                 <DialogTitle>Settings</DialogTitle>
                                 <DialogContent>
                                     <DialogContentText>
-                                        Enter the number of calendars you would like to generate along with a text file defining those calendars
+                                        Enter the number of calendars you would like to generate along with a text file defining those calendars.
                                     </DialogContentText>
                                     <Box
                                         sx={{
@@ -353,40 +356,38 @@ const MainUi = () => {
                                                 {numCalendars}
                                             </Typography>
                                         </Box>
-                                        <ButtonGroup 
+                                        <ButtonGroup
                                             disableElevation variant="contained"
                                         >
                                             <Button
                                                 onClick={() => setNumCalendars((numCalendars + 1))}
                                             >
-                                                <AddCircleIcon/>
+                                                <AddCircleIcon />
                                             </Button>
                                             <Button
                                                 onClick={handleDecrement}
                                             >
-                                                <RemoveCircleIcon/>
+                                                <RemoveCircleIcon />
                                             </Button>
                                         </ButtonGroup>
-                                        
+
                                     </Box>
                                 </DialogContent>
                                 <DialogActions>
                                     {/* Upload button */}
                                     <Button variant="contained" component="label" color="secondary">
-                                        <input type="file" hidden/>
-                                        <UploadFile/>
+                                        <input type="file" hidden />
+                                        <UploadFile />
                                     </Button>
                                     {/* Divider that alows left alignment for upload button */}
-                                    <div style={{flex: '1 0 0'}}></div> 
-                                    <Button variant="contained" onClick={handleClose}>Cancel</Button>
-                                    <Button variant="contained" onClick={handleClose}>Save</Button>
+                                    <div style={{ flex: '1 0 0' }}></div>
+                                    <Button variant="contained" onClick={handleCancel}>Cancel</Button>
+                                    <Button variant="contained" onClick={handleSave}>Save</Button>
                                 </DialogActions>
                             </Dialog>
                         </Grid>
                     </Grid>
-                
                 </Paper>
-                
             </Grid>
         </Grid>
     )
